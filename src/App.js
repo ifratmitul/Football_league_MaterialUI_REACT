1752dcd9fbffbf32a51e1d7333378d2b9bbc3342
ifratmitul <ifrat.mitul@hotmail.com>
2020-08-17 00:19:41 +0600

    row counting issue fixed, modal added, DONE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import Table from'./Table'
+
+
 function App() {
 
+  /*
+  
+  package installed  materila-table, react-bootstrap, react-modal
+  */
 
   return (
     <div className="App">
+     <h1>Task 1 and Task 2</h1>
+     <p>............................................</p>
       <Table/>
     </div>
   );
--- src/Table.js
+++ src/Table.js
@@ -1,26 +1,91 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MaterialTable from 'material-table'
 import PLdata from'./Tdata'
+import Modal from 'react-modal'
+import { ModalBody, ModalFooter } from 'react-bootstrap'
+import ModalHeader from 'react-bootstrap/esm/ModalHeader'
+
+Modal.setAppElement('#root')
 
 
 const Table =()=>{
     
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [team_name, setTeamName] = useState(0)
+    const [wins, setTeamWins] = useState(0)
+    const [draws, setTeamDraws] = useState(0)
+    const [losses, setTeamLosses] = useState(0)
+    const [played, setTeamPlayed] = useState(0)
 
-    const titleName = PLdata.map( item => item.name )
-    const matches = PLdata.map(item => item.rounds.map(i => i.matches.map(i=> (<p> <a href = "#">{i.team1}</a> vs <a href = "#">{i.team2}</a></p>))))
-    const matchDate = PLdata.map(item =>item.rounds.map(i => i.matches.map(i => (<p>{i.date}</p>))))
-    const scores = PLdata.map(item => item.rounds.map(i => i.matches.map(i =>(<p id = "s">{i.score.ft[0]} - {i.score.ft[1]}</p>))))
+    var titleName = PLdata.map( item => item.name )
+   
     
-        
+    // console.log('ok',PLdata[0]['rounds'])     
     
-    //console.log(scores)
-       const data = [
-                {   date: matchDate, 
-                    team: matches, 
-                    scores : scores
-                }
-                    ]
+    function abc(e){
+        setTeamName(e.target.id)
+        setTeamWins(stats[e.target.id]['win'])
+        setTeamDraws(stats[e.target.id]['draw'])
+        setTeamLosses(stats[e.target.id]['lost'])
+        setTeamPlayed(stats[e.target.id]['played'])
+        setModalIsOpen(true);
+    }
 
+    var data = []
+    var stats = {}
+    for (var i = 0; i < PLdata[0]['rounds'].length; i += 1){
+        // console.log('here', PLdata[0]['rounds'][i]['matches'])
+        for(var j = 0; j < PLdata[0]['rounds'][i]['matches'].length; j += 1){
+            var date_match = PLdata[0]['rounds'][i]['matches'][j]['date']
+            var team1 = PLdata[0]['rounds'][i]['matches'][j]['team1']
+            var team2 = PLdata[0]['rounds'][i]['matches'][j]['team2']//setTeam2(team1);setModalIsOpen(true);
+            var teams = <p><a href = "#" id={team1} onClick ={(e) =>  abc(e) }>{team1}</a> VS <a href = "#"id={team2} onClick ={(e) =>  abc(e) }>{team2}</a></p>//PLdata[0]['rounds'][i]['matches'][j]['team1'] + ' vs ' + PLdata[0]['rounds'][i]['matches'][j]['team2']
+            var sc1 = PLdata[0]['rounds'][i]['matches'][j]['score']['ft'][0]
+            var sc2 = PLdata[0]['rounds'][i]['matches'][j]['score']['ft'][1]
+            var score = sc1 + '-' + sc2
+            if (stats.hasOwnProperty(team1) === false){
+                stats[team1] = {}
+                stats[team1]['win']=0
+                stats[team1]['lost']=0
+                stats[team1]['draw']=0
+                stats[team1]['played']=0
+            }
+            if (stats.hasOwnProperty(team2) === false){
+                stats[team2] = {}
+                stats[team2]['win']=0
+                stats[team2]['lost']=0
+                stats[team2]['draw']=0
+                stats[team2]['played']=0
+            } 
+            if (sc1 > sc2){
+                stats[team1]['win'] += 1
+                stats[team2]['lost'] += 1 
+            }
+            else if (sc2 > sc1){
+                stats[team2]['win'] += 1
+                stats[team1]['lost'] += 1
+            }
+            else{
+                stats[team1]['draw'] += 1
+                stats[team2]['draw'] += 1
+            }
+            
+            stats[team1]['played']+=1
+            stats[team2]['played']+=1
+
+            var obj = {
+                date: date_match,
+                team: teams,
+                scores: score
+            }
+            data.push(obj)
+        }
+    }
+
+    // console.log(stats)
+       
+
+    // console.log(data);
     const columns = [
         {
           title : 'Date', field : 'date'
@@ -34,23 +99,71 @@ const Table =()=>{
     ]
     //Modal start
  
-    
     //Modal ends
-
 
 
     return (
         <div>
             <h1>{titleName}</h1>
+
             <MaterialTable 
             
             data = {data}
             columns = {columns}
-            options ={{search:false, showTitle: false} }
+            options ={{search:false, showTitle: false, pageSize : 10} }
             
             />
-   
+            
+            { <Modal isOpen = {modalIsOpen}
+            style={{
+                overlay: {
+                  position: 'fixed',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  backgroundColor: 'rgba(255, 255, 255, 0.75)'
+                },
+                content: {
+                  position: 'center',
+                  top: '40px',
+                  left: '40px',
+                  right: '40px',
+                  bottom: '40px',
+                  border: '1px solid #ccc',
+                  background: '#fff',
+                  overflow: 'auto',
+                  WebkitOverflowScrolling: 'touch',
+                  borderRadius: '4px',
+                  outline: 'none',
+                  padding: '20px'
+                }
+              }}>
+                <ModalHeader><h2>{team_name}</h2></ModalHeader>
+                
+
+                <ModalBody> 
+                    <table>
+                    <tr>
+                        <th>Played</th>
+                        <th>Win</th>
+                        <th>Los</th>
+                        <th>Draw</th>
+                    </tr>
+                    <tr>
+                        <td>{played}</td>
+                        <td>{wins}</td>
+                        <td>{losses}</td>
+                        <td>{draws}</td>
+                    </tr>
+                    
+                    </table></ModalBody>
+                <ModalFooter><button onClick ={() => setModalIsOpen(false)}>Close</button></ModalFooter>
+            </Modal>
+            
+    }
         </div>
+
     )
 
 
